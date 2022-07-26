40c35c7217fd5130069c3f414ce5f2473e338409
firatcanulukaya <mua.firat@gmail.com>
2022-07-26 09:20:49 +0300

    navigation

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {Ionicons} from "@expo/vector-icons";
+import ChatList from "./screens/ChatList";
+import Settings from "./screens/Settings";
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
+import Chat from "./screens/Chat";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const TabsNavigator = () => (
+    <Tabs.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+            return (
+                <Ionicons name={route.name === 'ChatList' ? 'chatbubbles' : 'settings'}
+                          size={size} color={color}/>
+            )
+        }
+    })}>
+        <Tabs.Screen name="ChatList" component={ChatList}/>
+        <Tabs.Screen name="Settings" component={Settings}/>
+    </Tabs.Navigator>
+)
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Main" component={TabsNavigator} options={{headerShown: false}}/>
+                <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
+                <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}}/>
+                <Stack.Screen name="Chat" component={Chat} options={{headerShown: false}}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from "react-native";
+
+const SignUp = () => {
+    return(
+        <Text>
+            signup
+        </Text>
+    )
+}
+
+export default SignUp;
