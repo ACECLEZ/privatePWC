ad22de0ca683d0cd9fccc5075d8e7597e6b9218b
firatcanulukaya <mua.firat@gmail.com>
2022-07-27 17:11:50 +0300

    sign in and sign up screens

--- screens/SignIn.jsx
+++ screens/SignIn.jsx
@@ -1,12 +1,75 @@
-import React from 'react';
-import {Text} from "react-native";
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity, SafeAreaView} from "react-native";
+import {colors} from "../config/constants";
+import {Ionicons} from "@expo/vector-icons";
 
-const SignIn = () => {
-    return(
-        <Text>
-            SignIn
-        </Text>
+const SignIn = ({navigation}) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+        <SafeAreaView>
+            <View style={styles.content}>
+                <Text style={styles.title}>Sign in to Account</Text>
+                <TextInput keyboardType="email-address" style={styles.input} placeholder="Email"
+                           onChangeText={text => setEmail(text)}/>
+                <TextInput textContentType="password" style={styles.input} secureTextEntry={true}
+                           placeholder="Password" onChangeText={text => setPassword(text)}/>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.text}>Do you need a account?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+                        <Text style={styles.textAlt}>Sign up.</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity style={styles.btn} activeOpacity={.7}>
+                    <Ionicons name="rocket-outline" color={colors.secondaryColor} size={16}/>
+                    <Text style={styles.btnText}>Sign In</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    content: {
+        padding: 32
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: '800',
+        color: colors.thirdColor,
+    },
+    input: {
+        backgroundColor: colors.secondaryColor,
+        fontSize: 15,
+        paddingHorizontal: 16,
+        paddingVertical: 14,
+        borderRadius: 6,
+        marginTop: 15,
+        marginBottom: 4,
+        color: colors.thirdColor
+    },
+    text: {
+        color: colors.secondaryTextColor,
+        marginRight: 5,
+        marginStart: 3
+    },
+    textAlt: {
+        color: colors.thirdColor,
+    },
+    btn: {
+        backgroundColor: colors.thirdColor,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 8,
+        borderRadius: 6,
+        marginTop: 20,
+        flexDirection: 'row'
+    },
+    btnText: {
+        color: colors.secondaryColor,
+        marginLeft: 8
+    }
+})
 
 export default SignIn;
--- screens/SignUp.jsx
+++ screens/SignUp.jsx
@@ -1,12 +1,78 @@
-import React from 'react';
-import {Text} from "react-native";
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity, SafeAreaView} from "react-native";
+import {colors} from "../config/constants";
+import {Ionicons} from "@expo/vector-icons";
 
-const SignUp = () => {
-    return(
-        <Text>
-            signup
-        </Text>
+const SignUp = ({navigation}) => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <SafeAreaView>
+            <View style={styles.content}>
+                <Text style={styles.title}>Create New Account</Text>
+                <TextInput style={styles.input} placeholder="Name Surname" onChangeText={text => setName(text)}/>
+                <TextInput keyboardType="email-address" style={styles.input} placeholder="Email"
+                           onChangeText={text => setEmail(text)}/>
+                <TextInput textContentType="password" style={styles.input} secureTextEntry={true}
+                           placeholder="Password" onChangeText={text => setPassword(text)}/>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.text}>Do you have account?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+                        <Text style={styles.textAlt}>Sign in.</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity style={styles.btn} activeOpacity={.7}>
+                    <Ionicons name="heart-outline" color={colors.secondaryColor} size={16}/>
+                    <Text style={styles.btnText}>Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    content: {
+        padding: 32
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: '800',
+        color: colors.thirdColor,
+    },
+    input: {
+        backgroundColor: colors.secondaryColor,
+        fontSize: 15,
+        paddingHorizontal: 16,
+        paddingVertical: 14,
+        borderRadius: 6,
+        marginTop: 15,
+        marginBottom: 4,
+        color: colors.thirdColor
+    },
+    text: {
+        color: colors.secondaryTextColor,
+        marginRight: 5,
+        marginStart: 3
+    },
+    textAlt: {
+        color: colors.thirdColor,
+    },
+    btn: {
+        backgroundColor: colors.thirdColor,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 8,
+        borderRadius: 6,
+        marginTop: 20,
+        flexDirection: 'row'
+    },
+    btnText: {
+        color: colors.secondaryColor,
+        marginLeft: 8
+    }
+})
 
 export default SignUp;
